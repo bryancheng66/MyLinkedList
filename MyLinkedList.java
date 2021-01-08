8fe180065132fb6e1d3052f832dbb029c71457e8
bryancheng66 <bryancheng66@gmail.com>
2021-01-08 00:45:58 -0500

    make add to index

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -27,5 +27,33 @@ public class MyLinkedList{
 		if (index < 0 || index > size){
 			throw new IndexOutOfBoundsException("Index " + index + " is out of bounds.");
 		}
+		
+		if (index == 0){
+			Node newNext = start;
+			start.setPrev(new Node(value));
+			start = start.getPrev();
+			start.setNext(newNext);
+		} else if (index == size){
+			Node newPrev = end;
+			end.setNext(new Node(value));
+			end = end.getNext();
+			end.setPrev(newPrev);
+		} else {
+			Node prevNode = findNthNode(index - 1);
+			Node nextNode = prevNode.getNext();
+			prevNode.setNext(new Node(value));
+			nextNode.setPrev(prevNode.getNext());
+		}
+		size++;
+	}
+
+	private Node findNthNode(int index){
+		Node current = start;
+		
+		for (int i = 0; i < index; i++){
+			current = current.getNext();
+		}
+
+		return current;
 	}
 }
